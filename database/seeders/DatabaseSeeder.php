7109df1e054030f812f559a6db0dc931b062ff8c
Muhammad-Rindy <muhammadrindy2705@gmail.com>
2024-01-30 23:30:06 +0700

    Updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,9 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Muhammad Rindy',
+            'email' => 'admin@admin.com',
+            'password'=> Hash::make('12345678'),
+        ]);
     }
 }
