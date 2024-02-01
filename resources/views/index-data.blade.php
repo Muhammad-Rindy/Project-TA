2a991f4589799a97e7f30a832ddb95b6effa7b27
Muhammad-Rindy <muhammadrindy2705@gmail.com>
2024-02-02 02:51:04 +0700

    updated files

--- resources/views/index-data.blade.php
+++ resources/views/index-data.blade.php
@@ -228,11 +228,11 @@
                                                     <select class="form-select mt-1 mb-1"
                                                         aria-label="Default select example" name="status">
                                                         @if ($product->status == '1')
-                                                            <option value="1" disabled>Active</option>
-                                                            <option value="0" selected>Not Available</option>
+                                                            <option value="1">Active</option>
+                                                            <option value="0">Not Available</option>
                                                         @else
-                                                            <option value="0" disabled>Not Available</option>
-                                                            <option value="1" selected>Active</option>
+                                                            <option value="0">Not Available</option>
+                                                            <option value="1">Active</option>
                                                         @endif
                                                     </select>
                                                     <div style="text-align: left">
